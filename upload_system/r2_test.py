ef1e1a137d0597124bb45f95ae716acb3f4064db
zengweis <105011744+zengweis@users.noreply.github.com>
2024-08-22 10:15:30 +0800

    Update r2_test.py

--- upload_system/r2_test.py
+++ upload_system/r2_test.py
@@ -2,9 +2,9 @@
 
 # 配置 Cloudflare R2 端点和凭证
 s3_client = boto3.client('s3',
-                         endpoint_url='https://ae7b21228c0af396feb1372330a8b87a.r2.cloudflarestorage.com',
-                         aws_access_key_id='4ebfc38641a6ea825bdbdf141d67b2f8',
-                         aws_secret_access_key='9a685c3fabb3a75d4894a6ecadf074afb9729d3bb3cba86646de1a28a3f5bea7')
+                         endpoint_url='https://xxxxxxxx.r2.cloudflarestorage.com',
+                         aws_access_key_id='xxxxxxxx',
+                         aws_secret_access_key='xxxxxxxx')
 
 # 列出指定存储桶中的对象
 bucket_name = 'anime-web-aliyun'
