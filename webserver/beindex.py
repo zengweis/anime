70bb18e3c09f825cb5821555b0766f60bd98c7d0
zengweis <105011744+zengweis@users.noreply.github.com>
2024-08-22 19:55:23 +0800

    Update beindex.py

--- webserver/beindex.py
+++ webserver/beindex.py
@@ -4,11 +4,11 @@ import pymysql
 
 # 配置 Cloudflare R2 端点和凭证
 s3_client = boto3.client('s3',
-                         endpoint_url='https://ae7b21228c0af396feb1372330a8b87a.r2.cloudflarestorage.com',
-                         aws_access_key_id='4ebfc38641a6ea825bdbdf141d67b2f8',
-                         aws_secret_access_key='9a685c3fabb3a75d4894a6ecadf074afb9729d3bb3cba86646de1a28a3f5bea7')
+                         endpoint_url='https://xxxxxxxxxxxxx.r2.cloudflarestorage.com',
+                         aws_access_key_id='xxxxxxxxxx',
+                         aws_secret_access_key='xxxxxxxxx')
 
-bucket_name = 'anime-web-aliyun'
+bucket_name = 'xxxxxxxxxxxxx'
 output_dir = '/www/wwwroot/anime/animelist'  # 视频页面保存的目录
 index_file_path = '/www/wwwroot/anime/index.html'   # index.html 保存的文件路径
 
